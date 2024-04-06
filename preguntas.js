4202ff6c4b437891d3078bfdc5b29961924b0ce5
elchirchito <pelaezbenjaminalejo@gmail.com>
2024-04-05 21:20:25 -0300

    primera subida

---
+++ preguntas.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "¿Cuantas copas Sudamericanas tiene Boca Juniors",
+    answer: "2",
+    options: [
+      "3",
+      "5",
+      "2",
+      "Ninguna es correcta"
+    ]
+  },
+    {
+    numb: 2,
+    question: ". ¿Quién es el máximo goleador de Boca Juniors?",
+    answer: "Palermo",
+    options: [
+      "Boyé",
+      "Varallo",
+      "Tevez",
+      "Palermo"
+    ]
+  },
+    {
+    numb: 3,
+    question: "¿Quién fue el DT más ganador de la historia de Boca Juniors?",
+    answer: "Carlos Bianchi",
+    options: [
+      "Alfio Basile",
+      "Carlos Bianchi",
+      "Marcelo Bielsa",
+      "Guillermo Barros Schelotto"
+    ]
+  },
+    {
+    numb: 4,
+    question: "¿Cuántas asistencias tiene Riquelme con la camiseta de Boca Juniors?",
+    answer: "123",
+    options: [
+      "123",
+      "145",
+      "98",
+      "183"
+    ]
+  },
+    {
+    numb: 5,
+    question: "¿Cuántos títulos nacionales tiene actualmente Boca Juniors?",
+    answer: "52",
+    options: [
+      "47",
+      "86",
+      "78",
+      "52"
+    ]
+  },
+  ];
